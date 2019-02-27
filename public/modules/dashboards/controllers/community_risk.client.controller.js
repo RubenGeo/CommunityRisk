75b9f72a1db2b591f1a1f075b1808827031c9f99
RubenGeo <rubenavalk@gmail.com>
2019-02-27 16:21:36 +0100

    Added WMS from docker and legend to cr controller

--- public/modules/dashboards/controllers/community_risk.client.controller.js
+++ public/modules/dashboards/controllers/community_risk.client.controller.js
@@ -2500,11 +2500,22 @@ angular.module("dashboards").controller("CommunityRiskController", [
 
       var countriesAndBoundaries = L.tileLayer
         .wms("http://localhost:8080/geoserver/FbF_Zambia/wms?", {
-          layers: "ESA_Clipped2",
+          layers: "ESA_Clipped",
           transparent: true,
           format: "image/png",
         })
         .addTo(map);
+
+      var legend = L.control({ position: "bottomright" });
+      legend.onAdd = function(map) {
+        var div = L.DomUtil.create("div", "info legend");
+        div.innerHTML +=
+          '<img src="http://localhost:8080/geoserver/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=FbF_Zambia:ESA_Clipped" alt="legend" width="145" height="130">';
+        return div;
+      };
+      legend.addTo(map);
+
+      console.log("hoihoih", legend);
 
       // if ($scope.directURLload) {
       // if ($scope.filters_url.length > 0) {
